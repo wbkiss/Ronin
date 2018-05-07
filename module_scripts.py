f919a1376c7bc62737defa7f2dac47d67625ca7c
ebrownin <ebrowninstc@yahoo.com>
2018-05-06 22:35:27 -0500

    Promoted Nessa and Alyss to mid-level admin

--- module_scripts.py
+++ module_scripts.py
@@ -54430,15 +54430,15 @@ scripts = [
 	   (player_get_unique_id,":player_uid",":player"),
 	   (try_begin),
 	      (this_or_next|eq,":player_uid",919212), # Hans
-		  (this_or_next|eq,":player_uid",1085169), # Alyss
 		  (this_or_next|eq,":player_uid",1123754), # Sky_Arrow
-		  (this_or_next|eq,":player_uid",1537560), # Nessa
-		  (this_or_next|eq,":player_uid",2125359), #HeXed/Cow
+		  (this_or_next|eq,":player_uid",2125359), # HeXed/Cow
 		  (eq,":player_uid",777777777777),
 		 (player_set_slot,":player",adimi_tool_admin_level_low,1),
 		 (multiplayer_send_2_int_to_player, ":player", adimi_tool_client_event, adimi_tool_set_admin_level,1),
 	   (else_try),
 	     (this_or_next|eq,":player_uid",1991520), # Dangel
+		 (this_or_next|eq,":player_uid",1537560), # Nessa
+		 (this_or_next|eq,":player_uid",1085169), # Alyss
 		 (this_or_next|eq,":player_uid",1259830), # Rebel_xls
 	     (this_or_next|eq,":player_uid",2939371), # Obedient_Duckbill
 	     (this_or_next|eq,":player_uid",667801), # Revelation/GK_Marquis
